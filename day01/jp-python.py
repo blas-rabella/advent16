84976eb1da03606e3e2a2ce7ba50b78d020c88bf
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2016-12-02 08:09:09 +0100

    WIP

--- day01/jp-python.py
+++ day01/jp-python.py
@@ -6,22 +6,27 @@ DIRECTIONS = ['L', 'R']
 with open('input.txt', 'r') as f:
     movements = f.read()
 
+
 movement_list = [(elem[0], int(elem[1])) for elem in movements.strip('\n ').split(', ')]
 
-get_dir = itemgetter(0)
+get_direction = itemgetter(0)
 get_distance = itemgetter(1)
+x = itemgetter(0)
+y = itemgetter(1)
 
-right_movements = [elem for elem in movement_list if get_dir(elem) == 'R']
-left_movements = [elem for elem in movement_list if get_dir(elem) == 'L']
 
-def distance(list_movements):
-    if len(list_movements) == 4:
-        d = abs(get_distance(list_movements[2]) - get_distance(list_movements[0])) + \
-            abs(get_distance(list_movements[3]) - get_distance(list_movements[1]))
+def get_distance_point(point):
 
-    if len(list_movements) == 3:
-        d = abs(get_length(list_movements[2]) - get_length(list_movements[0])) + \
-            get_length(list_movements[1])
+    direction = ''.join(get_direction(x(point), get_direction(y(point))))
+    directions_pair = {
+        'RL': (get_distance(x(point)), get_distance(y(point))),
+        'LR': (-get_distance(x(point)), get_distance(y(point))),
+        'LL': (-get_distance(x(point)), -get_distance(y(point))),
+        'RR': (get_distance(x(point)), -get_distance(y(point)))
+    }
 
-    if len(list_movements) == 2:
-        d  = sum(list_movements, key=get_distance)
+    return directions_pair[direction]
+
+
+def bloks_away(directions):
+    pass
