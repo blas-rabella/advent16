6f6cd98afe510105115914ba33ccd9e018385911
Blas Rabella Mariscal <blas.rabella@blueliv.com>
2016-12-02 12:20:45 +0100

    Day 2 done!

--- day2/blas-haskell.hs
+++ day2/blas-haskell.hs
@@ -3,26 +3,42 @@ import qualified Data.Matrix as M
 data Move = U | D | L | R deriving (Show, Read, Eq)
 type Point = (Int,Int)
 
-sub 0 = 0
+sub 1 = 1
 sub x = pred x
 
-add 2 = 2
+add 5 = 5
 add x = succ x
 
 doMove (x,y) m
-  | m == U = (x,sub y)
-  | m == D = (x,add y)
-  | m == L = (sub x, y)
-  | otherwise = (add x, y)
+  | m == U = (sub x, y)
+  | m == D = (add x, y)
+  | m == L = (x, sub y)
+  | otherwise = (x, add y)
 
-makeMoves :: Point -> [Move] -> Point
-makeMoves = foldl doMove
+doMove2 p m = if (pad2 M.! newPoint) /= "X" then newPoint else p
+  where newPoint = doMove p m
+  
+makeMoves :: (Point -> Move -> Point) -> Point -> [Move] -> Point
+makeMoves = foldl
 
-solveLine point line = makeMoves point instructions
+solveLine f point line = makeMoves f point instructions
 	where instructions = map (read . (:[])) line
 
-solve1 point lines = tail $ scanl solveLine point lines
+solve f point lines = tail $ scanl (solveLine f) point lines
+solve1 point lines = solve doMove point lines
+solve2 point lines = solve doMove2 point lines
 
 pad = M.fromList 3 3 [1..] 
 
-showSolution = map (\x -> pad ! x)
+pad2 = M.fromList 5 5 ["X", "X", "1", "X", "X"
+                      ,"X", "2", "3", "4", "X"
+                      ,"5", "6", "7", "8", "9"
+                      ,"X", "A", "B", "C", "X"
+                      ,"X", "X", "D", "X", "X"]
+
+
+showSolution = map (\x -> pad2 M.! x)
+ 
+main = do
+  text <- getContents
+  print . showSolution . solve2 (3,3). lines $ text
