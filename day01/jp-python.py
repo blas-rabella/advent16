04eb7fee64655204c65f76d520935511e7722d9a
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2016-12-04 23:45:09 +0100

    prpr

--- day01/jp-python.py
+++ day01/jp-python.py
@@ -1,13 +1,7 @@
+import operator
 from operator import itemgetter
+from functools import reduce
 
-
-DIRECTIONS = ['L', 'R']
-
-with open('input.txt', 'r') as f:
-    movements = f.read()
-
-
-movement_list = [(elem[0], int(elem[1])) for elem in movements.strip('\n ').split(', ')]
 
 get_direction = itemgetter(0)
 get_distance = itemgetter(1)
@@ -15,18 +9,57 @@ x = itemgetter(0)
 y = itemgetter(1)
 
 
-def get_distance_point(point):
-
-    direction = ''.join(get_direction(x(point), get_direction(y(point))))
-    directions_pair = {
-        'RL': (get_distance(x(point)), get_distance(y(point))),
-        'LR': (-get_distance(x(point)), get_distance(y(point))),
-        'LL': (-get_distance(x(point)), -get_distance(y(point))),
-        'RR': (get_distance(x(point)), -get_distance(y(point)))
-    }
-
-    return directions_pair[direction]
+DIRECTIONS = {
+    'N': (0, 1),
+    'S': (0, -1),
+    'E': (1, 0),
+    'W': (-1, 0)
+}
 
 
-def bloks_away(directions):
-    pass
+def get_orientation(mov, orientation):
+    op = getattr(operator, 'add')\
+        if get_direction(mov) == 'R' else getattr(operator, 'sub')
+
+    return op(orientation, 1) % 4
+
+
+def bloks_away(movements):
+    block = (0, 0)
+    orientation = 0
+    for mov in movements:
+        print('Actual block: ', block)
+        print('Mov: ', mov)
+        orientation = get_orientation(mov, orientation)
+        block = make_move(block, mov, orientation)
+        print('New block: ', block)
+        print('-' * 90 + '\n')
+
+    return abs(x(block)) + abs(y(block))
+
+
+def make_move(d_point, point, direction):
+    cardinal_points = ['N', 'E', 'S', 'W']
+    orientation = cardinal_points[direction]
+    return (x(d_point) + x(DIRECTIONS[orientation]) * int(get_distance(point)),
+            y(d_point) + y(DIRECTIONS[orientation]) * int(get_distance(point)))
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        movements = f.read()
+    # movements = [
+    #     'R4', 'R3', 'R5', 'L3', 'L5', 'R2', 'L2', 'R5', 'L2', 'R5', 'R5', 'R5', 'R1', 'R3', 'L2', 'L2',
+    #     'L1', 'R5', 'L3', 'R1', 'L2', 'R1', 'L3', 'L5', 'L1', 'R3', 'L4', 'R2', 'R4', 'L3', 'L1', 'R4',
+    #     'L4', 'R3', 'L5', 'L3', 'R188', 'R4', 'L1', 'R48', 'L5', 'R4', 'R71', 'R3', 'L2', 'R188',
+    #     'L3', 'R2', 'L3', 'R3', 'L5', 'L1', 'R1', 'L2', 'L4', 'L2', 'R5', 'L3', 'R3', 'R3', 'R4', 'L3',
+    #     'L4', 'R5', 'L4', 'L4', 'R3', 'R4', 'L4', 'R1', 'L3', 'L1', 'L1', 'R4', 'R1', 'L4', 'R1', 'L1',
+    #     'L3', 'R2', 'L2', 'R2', 'L1', 'R5', 'R3', 'R4', 'L5', 'R2', 'R5', 'L5', 'R1', 'R2', 'L1', 'L3',
+    #     'R3', 'R1', 'R3', 'L4', 'R4', 'L4', 'L1', 'R1', 'L2', 'L2', 'L4', 'R1', 'L3', 'R4', 'L2', 'R3',
+    #     'L1', 'L5', 'R4', 'R5', 'R2', 'R5', 'R1', 'R5', 'R1', 'R3', 'L3', 'L2', 'L2', 'L5', 'R2', 'L2',
+    #     'R5', 'R5', 'L2', 'R3', 'L5', 'R5', 'L2', 'R4', 'R2', 'L1', 'R3', 'L5', 'R3', 'R2', 'R5', 'L1',
+    #     'R3', 'L2', 'R2', 'R1'
+    # ]
+    movements = [mov for mov in movements.strip('\n').split(', ')]
+
+    print(bloks_away(movements))
